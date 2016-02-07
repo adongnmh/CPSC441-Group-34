b5b2ad063622b85ba01af9f15abe617456b6066c
Andrew Dong <andrew.mh.dong@gmail.com>
2016-02-07 16:40:24 -0700

    Project Files.

---
+++ PROJECT/CollaborativeDrawing/src/Logic/CanvasLogic.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Logic;
+
+/**
+ * This class will handle all logic for the drawing portion of the application 
+ * 
+ * @author Andrew Dong
+ * Date Last Modified: 02/07/2016
+ */
+public class CanvasLogic {
+    
+}
---
+++ PROJECT/CollaborativeDrawing/src/collaborativedrawing/CollaborativeDrawing.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package collaborativedrawing;
+import  GUI.*;
+
+/**
+ *
+ * @author Andrew Dong
+ */
+public class CollaborativeDrawing 
+{
+
+    public static void main(String[] args) 
+    {
+        // TODO code application logic here
+        DrawingScreen newCanvas = new DrawingScreen();
+        newCanvas.setVisible(true);
+    }
+    
+}
