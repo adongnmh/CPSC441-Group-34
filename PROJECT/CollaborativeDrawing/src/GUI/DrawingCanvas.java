2a2edd087fe6eb9093f0ff2a36a655636a63dc76
Andrew Dong <andrew.mh.dong@gmail.com>
2016-02-15 14:54:54 -0700

    Re organizing folders

--- PROJECT/CollaborativeDrawing/src/GUI/DrawingCanvas.java
+++ PROJECT/CollaborativeDrawing/src/GUI/DrawingCanvas.java
@@ -15,132 +15,143 @@ import java.awt.event.MouseEvent;
 import java.awt.event.MouseMotionAdapter;
 
 /**
- * Class will contains the JPANEL that will be put onto the Jframe that we initialized in main
- * 
- * @author CPSC Group 34
- * Date Last Modified: 02/07/2016
+ * Class will contains the JPANEL that will be put onto the Jframe that we
+ * initialized in main
+ *
+ * @author CPSC Group 34 Date Last Modified: 02/07/2016
  */
-public class DrawingCanvas extends javax.swing.JPanel 
+public class DrawingCanvas extends javax.swing.JPanel
 {
+
     // Image that we will draw using the paint.
     private Image image;
     // Graphics2D object this is what we will use to draw on
     private Graphics2D drawing;
 
-    //Corrdinates of the mouse X/Y old and new coordinates used 
+    //Corrdinates of the mouse X/Y old and new coordinates used
     // for drawing the image of the user
     private int oldXCoord;
     private int oldYCoord;
     private int currentXCoord;
     private int currentYCoord;
+    private String paintColor;
 
     /**
      * Creates new form DrawingCanvase
+     *
+     * @param currentFrame
      */
-    public DrawingCanvas() 
+    public DrawingCanvas(DrawingScreenFrame currentFrame)
     {
         // Initializing the JPANEL this is done automatically by netbeans design.
         // DO NOT touch the initCOmponents method!
         initComponents();
-        
+
+        paintColor = currentFrame.getColor();
+        System.out.println(paintColor);
+        if ("blue".equals(paintColor))
+        {
+            drawing.setPaint(Color.blue);
+        }
+
         // Listening for whenever the mouse is pressed. If the mouse is pressed,
-        // when pressed we will know where on the canvas the line will start using 
+        // when pressed we will know where on the canvas the line will start using
         // the get methods.
         //SOURCE: http://www.tutorialspoint.com/awt/awt_mouseadapter.htm
-        addMouseListener(new MouseAdapter() 
+        addMouseListener(new MouseAdapter()
         {
-            public void mousePressed(MouseEvent e) 
+            public void mousePressed(MouseEvent e)
             {
-              // save coord x,y when mouse is pressed
-              oldXCoord = e.getX();
-              oldYCoord = e.getY();
+                // save coord x,y when mouse is pressed
+                oldXCoord = e.getX();
+                oldYCoord = e.getY();
             }
         });
 
         // Listening for whenever the mouse is being dragged across the JPanel
         // new cordinates of the image/line will be calculated using the get methods
         // SOURCE: http://www.tutorialspoint.com/awt/awt_mouseadapter.htm
-        addMouseMotionListener(new MouseMotionAdapter() 
+        addMouseMotionListener(new MouseMotionAdapter()
         {
-          public void mouseDragged(MouseEvent e) 
-          {
-            // coord x,y when drag mouse
-            currentXCoord = e.getX();
-            currentYCoord = e.getY();
-
-            if (drawing != null) 
+            public void mouseDragged(MouseEvent e)
             {
-              // draw line if drawing context not null
-              drawing.drawLine(oldXCoord, oldYCoord, currentXCoord, currentYCoord);
-              // refresh draw area to repaint
-              repaint();
+                // coord x,y when drag mouse
+                currentXCoord = e.getX();
+                currentYCoord = e.getY();
 
-              // Storing the old x and y coordinates as the current x and y 
-              // coordinates
-              oldXCoord = currentXCoord;
-              oldYCoord = currentYCoord;
+                if (drawing != null)
+                {
+                    // draw line if drawing context not null
+                    drawing.drawLine(oldXCoord, oldYCoord, currentXCoord, currentYCoord);
+                    // refresh draw area to repaint
+                    repaint();
+
+                    // Storing the old x and y coordinates as the current x and y
+                    // coordinates
+                    oldXCoord = currentXCoord;
+                    oldYCoord = currentYCoord;
+                }
             }
-          }
         });
-        
 
         // Listening for whenever the mouse is being dragged across the JPanel
         // new cordinates of the image/line will be calculated using the get methods
         // SOURCE: http://www.tutorialspoint.com/awt/awt_mouseadapter.htm
-        addMouseListener(new MouseAdapter() 
+        addMouseListener(new MouseAdapter()
         {
             public void mouseClicked(MouseEvent e)
             {
-            // coord x,y when drag mouse
-            currentXCoord = e.getX();
-            currentYCoord = e.getY();
+                // coord x,y when drag mouse
+                currentXCoord = e.getX();
+                currentYCoord = e.getY();
 
-            if (drawing != null) 
-            {
-              // draw line if drawing context not null
-              drawing.drawLine(oldXCoord, oldYCoord, currentXCoord, currentYCoord);
-              // refresh draw area to repaint
-              repaint();
+                if (drawing != null)
+                {
+                    // draw line if drawing context not null
+                    drawing.drawLine(oldXCoord, oldYCoord, currentXCoord, currentYCoord);
+                    // refresh draw area to repaint
+                    repaint();
 
-              // Storing the old x and y coordinates as the current x and y 
-              // coordinates
-              oldXCoord = currentXCoord;
-              oldYCoord = currentYCoord;
+                    // Storing the old x and y coordinates as the current x and y
+                    // coordinates
+                    oldXCoord = currentXCoord;
+                    oldYCoord = currentYCoord;
+                }
+
             }
-                
-            }
-            
+
         });
 
     }
-    
-    protected void paintComponent(Graphics g) 
+
+    protected void paintComponent(Graphics g)
     {
-        if (image == null) {
-          // image to draw null initializing the image
-          image = createImage(getSize().width, getSize().height);
-          drawing = (Graphics2D) image.getGraphics();
-          // enable antialiasing - smoother Lines
-          drawing.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-          // clear draw area
-          clear();
+        if (image == null)
+        {
+            // image to draw null initializing the image
+            image = createImage(getSize().width, getSize().height);
+            drawing = (Graphics2D) image.getGraphics();
+            // enable antialiasing - smoother Lines
+            drawing.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+            // clear draw area
+            clear();
         }
 
         g.drawImage(image, 0, 0, null);
-  }
- 
-  /**
-   * Clear method will clear the whole canvas. This will be implemented with the
-   * clear button
-   * Date Last Modified: 02/07/2016
-   */
-  public void clear() {
-    drawing.setPaint(Color.white);
-    // draw white on entire draw area to clear
-    drawing.fillRect(0, 0, getSize().width, getSize().height);
-    drawing.setPaint(Color.black);
-    repaint();
-  }
+    }
+
+    /**
+     * Clear method will clear the whole canvas. This will be implemented with
+     * the clear button Date Last Modified: 02/07/2016
+     */
+    public void clear()
+    {
+        drawing.setPaint(Color.white);
+        // draw white on entire draw area to clear
+        drawing.fillRect(0, 0, getSize().width, getSize().height);
+        drawing.setPaint(Color.black);
+        repaint();
+    }
 
     /**
      * This method is called from within the constructor to initialize the form.
@@ -164,7 +175,6 @@ public class DrawingCanvas extends javax.swing.JPanel
             .addGap(0, 420, Short.MAX_VALUE)
         );
     }// </editor-fold>//GEN-END:initComponents
-
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     // End of variables declaration//GEN-END:variables
