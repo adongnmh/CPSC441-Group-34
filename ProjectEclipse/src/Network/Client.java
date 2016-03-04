3821c7a815c445f28fa4453a6bb044dbe31dc9d8
Andrew Dong <andrew.mh.dong@gmail.com>
2016-03-04 02:28:23 -0700

    Final Push + UML Diagram

---
+++ ProjectEclipse/src/Network/Client.java
@@ -0,0 +1,19 @@
+package Network;
+
+public class Client {
+
+	public void RequestConnection()
+	{
+		
+	}
+	
+	public void SendRequest()
+	{
+		
+	}
+	
+	public void ClientDisconnect()
+	{
+		
+	}
+}
---
+++ ProjectEclipse/src/Network/Server.java
@@ -0,0 +1,23 @@
+package Network;
+
+public class Server {
+
+	private int messageCode;
+	
+	
+	public void ConnectionRequestAnswer()
+	{
+		
+	}
+	
+	public int getMessageCode()
+	{
+		return messageCode;
+	}
+	
+	public void CloseServer()
+	{
+		
+	}
+	
+}
