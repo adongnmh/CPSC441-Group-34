88203218ef9e7085938937128f16099412c36c8a
Charlie Cheung <charliecheung@users.noreply.github.com>
2016-04-02 19:29:53 -0600

    update interface for applicationMainScreen.java
    workwork

--- ProjectEclipse/src/GUI/ApplicationMainScreen.java
+++ ProjectEclipse/src/GUI/ApplicationMainScreen.java
@@ -17,7 +17,7 @@ public class ApplicationMainScreen extends JFrame {
 	 */
 	public ApplicationMainScreen() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 516, 485);
+		setBounds(100, 100, 871, 530);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
