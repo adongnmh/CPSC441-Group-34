e192198fde440e3bafef94e057a1931169712af9
Andrew Dong <andrew.mh.dong@gmail.com>
2016-04-03 21:12:22 -0600

    NEW CLASS

---
+++ ProjectEclipse/src/GUI/ConnectScreen.java
@@ -0,0 +1,81 @@
+package GUI;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.JTextField;
+import javax.swing.LayoutStyle.ComponentPlacement;
+import javax.swing.JButton;
+
+public class ConnectScreen extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField ipField;
+	private JTextField portField;
+	private JButton btnConnect; 
+	
+	public ConnectScreen() 
+	{
+		InitializeConnectScreen();
+	}
+	
+	public void InitializeConnectScreen()
+	{
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 535, 425);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		
+		JLabel lblNewLabel = new JLabel("IP ADDRESS");
+		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 12));
+		
+		JLabel lblPortNumber = new JLabel("PORT NUMBER");
+		lblPortNumber.setFont(new Font("Tahoma", Font.BOLD, 12));
+		
+		ipField = new JTextField();
+		ipField.setColumns(10);
+		
+		portField = new JTextField();
+		portField.setColumns(10);
+		
+		btnConnect = new JButton("CONNECT");
+		btnConnect.setFont(new Font("Tahoma", Font.BOLD, 11));
+		GroupLayout gl_contentPane = new GroupLayout(contentPane);
+		gl_contentPane.setHorizontalGroup(
+			gl_contentPane.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addGap(176)
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.TRAILING, false)
+						.addComponent(btnConnect, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+						.addComponent(portField, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 156, Short.MAX_VALUE)
+						.addComponent(lblPortNumber, Alignment.LEADING, GroupLayout.PREFERRED_SIZE, 142, GroupLayout.PREFERRED_SIZE)
+						.addComponent(lblNewLabel, Alignment.LEADING)
+						.addComponent(ipField, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 156, Short.MAX_VALUE))
+					.addContainerGap(177, Short.MAX_VALUE))
+		);
+		gl_contentPane.setVerticalGroup(
+			gl_contentPane.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addGap(73)
+					.addComponent(lblNewLabel)
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addComponent(ipField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+					.addGap(31)
+					.addComponent(lblPortNumber, GroupLayout.PREFERRED_SIZE, 15, GroupLayout.PREFERRED_SIZE)
+					.addPreferredGap(ComponentPlacement.UNRELATED)
+					.addComponent(portField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+					.addGap(61)
+					.addComponent(btnConnect, GroupLayout.PREFERRED_SIZE, 72, GroupLayout.PREFERRED_SIZE)
+					.addContainerGap(52, Short.MAX_VALUE))
+		);
+		contentPane.setLayout(gl_contentPane);
+	}
+}
